60e9be93bf461ffa0fd10150427c618cdae087c0
JRBros2346 <jrbros2346@gmail.com>
2024-09-01 16:29:14 +0530

    [Add] OpenWebText

--- backend/model/kIwI-GPT/kiwi.py
+++ backend/model/kIwI-GPT/kiwi.py
@@ -2,92 +2,83 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 import pickle
-import csv
-from transformers import AutoTokenizer
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 block_size = 128
-batches = 32
-train_loop = 5000
-learning_rate = 5e-4
+batches = 64
+train_loop = 3000
+learning_rate = 3e-4
 train_split = 0.8
-debug_loop = 500
+debug_loop = 100
 embeds = 512
-heads = 4
-layers = 4
+heads = 8
+layers = 8
 dropout = 0.2
 
-tokenizer = AutoTokenizer.from_pretrained('gpt2')
-vocab_size = len(tokenizer)
+# tokenizer = AutoTokenizer.from_pretrained('gpt2')
+# vocab_size = len(tokenizer)
 
-encode = lambda s: tokenizer.encode(s, add_special_tokens=True)
-decode = lambda l: tokenizer.decode(l, skip_special_tokens=True)
+# encode = lambda s: tokenizer.encode(s, add_special_tokens=True)
+# decode = lambda l: tokenizer.decode(l, skip_special_tokens=True)
 
-# chars = []
-# text = ""
-# with open('datasets/dialogs.csv', 'r', encoding='utf-8') as f:
-#     reader = csv.reader(f, delimiter='\t')
-#     for row in reader:
-#         text += f"developer: {row[0]}\nkivi: {row[1]}\n"
-#     chars = sorted(set(text))
-# print(text[:100])
-# print(chars)
-# vocab_size = len(chars)
-# print(vocab_size)
+chars = ""
+with open('datasets/shakespearean.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+    chars = sorted(set(text))
+print(text[:100])
+print(chars)
+vocab_size = len(chars)
+print(vocab_size)
 
-# str2int = { ch:i for i,ch in enumerate(chars) }
-# int2str = { i:ch for i,ch in enumerate(chars) }
-# encode = lambda s: [ str2int[c] for c in s.lower() ]
-# decode = lambda l: ''.join([ int2str[i] for i in l ])
-# data = torch.tensor(encode(text), dtype=torch.long).to(device)
-# print(data[:100])
+str2int = { ch:i for i,ch in enumerate(chars) }
+int2str = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [ str2int[c] for c in s.lower() ]
+decode = lambda l: ''.join([ int2str[i] for i in l ])
+data = torch.tensor(encode(text), dtype=torch.long).to(device)
+print(data[:100])
 
-def load_half_dataset(file):
-    with open(file, 'r', encoding='utf-8') as f:
-        f.seek(0, 2)
-        half = f.tell() // 200
-        f.seek(0)
-        data = f.read(half)
-    return data
+n = int(train_split*len(data))
+train = data[:n]
+valid = data[n:]
 
-train_data = load_half_dataset("datasets/shakespearean.txt")
-val_data = load_half_dataset("datasets/shakespearean.txt")
-encoded_train = torch.tensor(encode(train_data), dtype=torch.long)
-encoded_val = torch.tensor(encode(val_data), dtype=torch.long)
+# def load_half_dataset(file):
+#     with open(file, 'r', encoding='utf-8') as f:
+#         f.seek(0, 2)
+#         half = f.tell() // 200
+#         f.seek(0)
+#         data = f.read(half)
+#     return data
 
-# n = int(train_split*len(data))
-# train = data[:n]
-# valid = data[n:]
+# train_data = load_half_dataset("datasets/shakespearean.txt")
+# val_data = load_half_dataset("datasets/shakespearean.txt")
+# train = torch.tensor(encode(train_data), dtype=torch.long)
+# valid = torch.tensor(encode(val_data), dtype=torch.long)
+
 
 def get_batch(split):
-    data = encoded_train if split=='train' else encoded_val
-    if data.size(0) > block_size:
-        ix = torch.randint(0, data.size(0) - block_size, (batches,))
-        x = torch.stack([data[i:i+block_size] for i in ix])
-        y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    else:
-        raise ValueError("Dataset size is too small for the requested block and batch sizes.")
+    data = train if split=='train' else valid
+    ix = torch.randint(0, data.size(0) - block_size, (batches,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
     x, y = x.to(device), y.to(device)
     return x, y
 
-x, y = get_batch('train')
-print('inputs:')
-print(x)
-print('target:')
-print(y)
+# x, y = get_batch('train')
+# print('inputs:')
+# print(x)
+# print('target:')
+# print(y)
 
-class MHFlashAttn(nn.Module):
+class Head(nn.Module):
     def __init__(self, head_size):
         super().__init__()
-        self.head_size = head_size
-        self.key = nn.Linear(embeds//heads, head_size, bias=False, dtype=torch.float16)
-        self.query = nn.Linear(embeds//heads, head_size, bias=False, dtype=torch.float16)
-        self.value = nn.Linear(embeds//heads, head_size, bias=False, dtype=torch.float16)
+        self.key = nn.Linear(embeds, head_size, bias=False)
+        self.query = nn.Linear(embeds, head_size, bias=False)
+        self.value = nn.Linear(embeds, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
         self.dropout = nn.Dropout(dropout)
     def forward(self, x):
         B, T, C = x.shape
-        x = x.view(B, T, heads, C//heads)
         k = self.key(x)
         q = self.query(x)
         wei = q @ k.transpose(-2, -1) * k.shape[-1]**-.5
@@ -101,11 +92,11 @@ class MHFlashAttn(nn.Module):
 class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
-        self.mha_flash_attn = MHFlashAttn(head_size)
-        self.proj = nn.Linear(head_size, embeds)
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, embeds)
         self.dropout = nn.Dropout(dropout)
     def forward(self, x):
-        out = self.mha_flash_attn(x)
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
         out = self.dropout(self.proj(out))
         return out
 
@@ -114,7 +105,7 @@ class FeedForward(nn.Module):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(embeds, 4*embeds),
-            nn.GELU(),
+            nn.ReLU(),
             nn.Linear(4*embeds, embeds),
             nn.Dropout(dropout),
         )
@@ -124,15 +115,15 @@ class FeedForward(nn.Module):
 class Block(nn.Module):
     def __init__(self, embeds, heads):
         super().__init__()
-        head_size = embeds
+        head_size = embeds // heads
         # x_size = torch.tensor([batches, block_size, heads, embeds])
         self.sa = MultiHeadAttention(heads, head_size)
         self.ffwd = FeedForward(embeds)
-        self.ln1 = nn.LayerNorm(embeds, head_size, dtype=torch.float16)
-        self.ln2 = nn.LayerNorm(embeds, head_size, dtype=torch.float16)
+        self.ln1 = nn.LayerNorm(embeds)
+        self.ln2 = nn.LayerNorm(embeds)
     def forward(self, x):
         y = self.sa(x)
-        print(y.shape)
+        # print(y.shape)
         x = self.ln1(x + y)
         y = self.ffwd(x)
         x = self.ln2(x + y)
@@ -141,8 +132,8 @@ class Block(nn.Module):
 class Kiwi(nn.Module):
     def __init__(self, vocab_size):
         super().__init__()
-        self.token_embedding = nn.Embedding(vocab_size, embeds, dtype=torch.float16)
-        self.position_embedding = nn.Embedding(block_size, embeds, dtype=torch.float16)
+        self.token_embedding = nn.Embedding(vocab_size, embeds)
+        self.position_embedding = nn.Embedding(block_size, embeds)
         self.blocks = nn.Sequential(*[Block(embeds, heads=heads) for _ in range(layers)])
         self.ln_final = nn.LayerNorm(embeds)
         self.head = nn.Linear(embeds, vocab_size)
@@ -194,13 +185,8 @@ def estimate_loss(model):
     for split in ['train', 'val']:
         losses = torch.zeros(debug_loop)
         for k in range(debug_loop):
-            if encoded_val.size(0) > block_size:
-                ix = torch.randint(0, encoded_val.size(0) - block_size, (batches,))
-                x = torch.stack([encoded_val[i:i+block_size] for i in ix])
-                x = torch.stack([encoded_val[i+1:i+block_size+1] for i in ix])
-            else:
-                raise ValueError("Dataset size is too small for the requested block and batch sizes.")
-            logits, loss = model(x.to(device), y.to(device))
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
             losses[k] = loss.item()
         out[split] = losses.mean()
     model.train()
@@ -209,19 +195,16 @@ def estimate_loss(model):
 def train_model(model, train_loop, debug_loop):
     optim = torch.optim.AdamW(model.parameters(), lr=learning_rate)
     for iter in range(train_loop):
+        print(f"\r{iter}", end='')
         if iter % debug_loop == 0:
             losses = estimate_loss(model)
             print(f'step: {iter}, train loss: {losses['train']:.6f}, valid loss: {losses['val']:.6f}')
-        if encoded_train.size(0) > block_size:
-            ix = torch.randint(0, encoded_train.size(0) - block_size, (batches,))
-            x = torch.stack([encoded_train[i:i+block_size] for i in ix])
-            y = torch.stack([encoded_train[i+1:i+block_size+1] for i in ix])
-        else:
-            raise ValueError("Dataset size is too small for the requested block and batch sizes.")
-        logits, loss = model.forward(x.to(device), y.to(device))
+        xb, yb = get_batch('train')
+        logits, loss = model.forward(xb, yb)
         optim.zero_grad(set_to_none=True)
         loss.backward()
         optim.step()
+    print(iter+1)
     print(loss.item())
 
 def generate(model, max_tokens, prompt=None):
@@ -231,15 +214,15 @@ def generate(model, max_tokens, prompt=None):
     print(generated)
     return generated
 
-def export_model(model, file):
-    with open(file, 'wb') as f:
+def export_model(model, name):
+    with open(f"models/{name}.pkl", 'wb') as f:
         pickle.dump(model, f)
 
-def import_model(file):
-    return pickle.load(open(file, 'rb'))
+def import_model(name):
+    return pickle.load(open(f"models/{name}.pkl", 'rb'))
 
 model = Kiwi(vocab_size).to(device)
-# model = import_model('models/dialogs.pkl')
-train_model(model, train_loop, debug_loop)
-export_model(model, 'models/shakespear.pkl')
-open('generated/shakespear-kivi.txt', 'a').write(generate(model, 100, prompt='Developer: What is your name?\n'))
+model = import_model('dialogs')
+# train_model(model, train_loop, debug_loop)
+# export_model(model, 'shakespearean')
+open('generated/shakespearean-test-kivi.txt', 'a').write(generate(model, 1000, prompt="I am William Shakespear"))
